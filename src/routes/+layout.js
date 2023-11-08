de37ddc7a77828f2fa1e2644d945aa0c9478144a
Tarpstone <mike@tarpey.dev>
2023-11-07 21:20:22 -0500

    Might not need this anymore since we're off S3

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,4 @@
 // for static site export
 export const prerender = true;
 // for S3 specifically -- make sure /deep redirects to /deep/index.html
-export const trailingSlash = 'always';
+// export const trailingSlash = 'always';
