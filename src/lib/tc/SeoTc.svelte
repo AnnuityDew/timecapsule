133bede1484e6f1248ab7c6469d09b3779dc80c2
Tarpstone <mike@tarpey.dev>
2023-11-08 00:29:14 -0500

    Adding global SEO

--- src/lib/tc/SeoTc.svelte
+++ src/lib/tc/SeoTc.svelte
@@ -6,19 +6,6 @@
 <svelte:head>
 	<title>Mike Tarpey{title}</title>
 	<meta NAME="Description" CONTENT={description} />
-	<meta name="author" content="Mike Tarpey" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- these keywords were only added in the high school version of the site -->
 	<meta NAME="Keywords" CONTENT="michael mike patrick tarpey college senior uconn irish landstown class of 2010" />
-	<!-- favicon stuff -->
-	<meta name="msapplication-TileColor" content="#2d89ef" />
-	<meta name="theme-color" content="#ffffff" />
-	<link rel="apple-touch-icon" sizes="180x180"
-	  href="/index/favicon/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32"
-	  href="/index/favicon/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16"
-	  href="/index/favicon/favicon-16x16.png" />
-	<link rel="manifest" href="/index/favicon/site.webmanifest" />
-	<link rel="mask-icon" href="/index/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-	<link rel="shortcut icon" href="/index/favicon/favicon.ico" />
 </svelte:head>
